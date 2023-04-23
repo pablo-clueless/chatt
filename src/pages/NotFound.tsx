d8cb5320a7e14f182abe4663f0134335a7023afa
Samson Okunola <smsnmicheal@gmail.com>
2023-04-23 14:10:47 +0100

    fix: fixed text position in `NotFound`

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,10 +6,10 @@ const NotFound = () => {
   return (
     <>
     <Navbar />
-    <main className='w-full h-[607px] grid place-items-center bg-gray-200 bg-not-found bg-no-repeat bg-contain bg-center px-4'>
-      <div className='w-full md:w-[400px] flex flex-col items-center bg-gray-400/50 rounded-md p-5'>
+    <main className='w-full h-[607px] flex flex-col items-center bg-gray-200 bg-not-found bg-no-repeat bg-contain bg-center px-4'>
+      <div className='w-full md:w-[400px] flex flex-col items-center rounded-md p-5 mt-5'>
         <p className='text-9xl text-gray-700 font-black tracking-wider'>404</p>
-        <Link to='/' className='text-gray-700 text-xl font-medium underline mt-5'>Go Home</Link>
+        <Link to='/' className='text-white text-xl font-medium underline mt-5'>Go Home</Link>
       </div>
     </main>
     <Footer />
